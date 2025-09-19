5a566e4bced27c62d97892ca16361bc53a9083af
Hary <azharimad@users.noreply.github.com>
2025-09-19 17:00:31 +0700

    Create privacy.vue

---
+++ pages/privacy.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useSeo({ title: 'Terms of Service — Sorabit' }) // ganti per halaman
+</script>
+<template>
+  <section class="prose max-w-3xl">
+    <h1>Privacy</h1>
+    <p>Isi ToS di sini.</p>
+  </section>
+</template>
