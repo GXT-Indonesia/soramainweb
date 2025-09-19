fe76740e083b8364d6423271463e6152c9f43b96
Hary <azharimad@users.noreply.github.com>
2025-09-19 16:59:45 +0700

    Create tos.vue

---
+++ pages/tos.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useSeo({ title: 'Terms of Service — Sorabit' }) // ganti per halaman
+</script>
+<template>
+  <section class="prose max-w-3xl">
+    <h1>Terms of Service</h1>
+    <p>Isi ToS di sini.</p>
+  </section>
+</template>
