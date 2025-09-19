6e9430009f7e4aa1406a11ea9b30b1c223c11040
Hary <azharimad@users.noreply.github.com>
2025-09-19 16:53:09 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  routeRules: {
-    // prerender index route by default
-    '/': { prerender: true },
+  modules: ['@nuxtjs/robots', '@nuxtjs/sitemap', '@nuxt/image-edge'],
+  css: ['~/assets/css/tailwind.css'],
+  site: { url: 'https://sorabit.com', name: 'Sorabit' },
+  nitro: {
+    prerender: { crawlLinks: true },
+    routeRules: {
+      // halaman “produk” bisa di-ISR 10 menit
+      '/(hosting|hosting-murah|domain|cloud-vps|cdn|email-hosting|colocation|dedicated-server|ssl)': { isr: 600 },
+      '/**': { headers: { 'cache-control': 'public, max-age=300, s-maxage=600, stale-while-revalidate=86400' } }
+    }
   },
-});
+  image: { provider: 'ipx' }
+})
